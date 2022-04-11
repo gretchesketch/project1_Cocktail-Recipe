0331a65a1931206e5c97291eded4e08b3d95841a
Jason Chun <jchun711@gmail.com>
2022-04-11 15:00:12 -0400

    trying to git pull new feature from dynamic cards branch.

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 // images display on landing page
-const img = document.querySelector('.img');
+const img = document.querySelector(".img");
 const cycleImages = [
-    './images/bluedrink.jpg', './images/greendrink.jpg', './images/pinkdrink.jpg', './images/2drinks.jpg', './images/2drinksGreenAndRed.jpg'
+  "./images/bluedrink.jpg",
+  "./images/greendrink.jpg",
+  "./images/pinkdrink.jpg",
+  "./images/2drinks.jpg",
+  "./images/2drinksGreenAndRed.jpg",
 ];
 // carousel
 async function cycle() {
   img.src = cycleImages[0];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[1];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[2];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[3];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[4];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   cycle();
 }
 cycle();
 
-
-var cockTails = document.querySelector('ul');
-var searchBtn = document.querySelector('searchbtn')
+var cockTails = document.querySelector("ul");
+var searchBtn = document.querySelector("searchbtn");
 
 // function to fetch api
 function getApi() {
-    var requestUrl = '';
+  var requestUrl = "";
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var listItem = document.createElement('li');
-                listItem.textContent = data[i].html_url;
-                cockTails.appendChild(listItem);
-            }
-        });
-};
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = data[i].html_url;
+        cockTails.appendChild(listItem);
+      }
+    });
+}
 
-searchBtn.addEventListener('click', getApi);
+searchBtn.addEventListener("click", getApi);
