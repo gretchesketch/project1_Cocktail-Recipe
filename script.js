bb664892b913328983a36aaa4aa3504c24c34b9f
gretchesketch <gretchenjensen92@gmail.com>
2022-04-13 12:56:31 -0400

    changed the code over to what is in the dynamic cards branch

--- script.js
+++ script.js
@@ -1,43 +1,141 @@
 // images display on landing page
-const img = document.querySelector('.img');
+const img = document.querySelector(".img");
 const cycleImages = [
-    './images/bluedrink.jpg', './images/greendrink.jpg', './images/pinkdrink.jpg', './images/2drinks.jpg', './images/2drinksGreenAndRed.jpg'
+  "./images/bluedrink.jpg",
+  "./images/greendrink.jpg",
+  "./images/pinkdrink.jpg",
+  "./images/2drinks.jpg",
+  "./images/2drinksGreenAndRed.jpg",
 ];
+
 // carousel
 async function cycle() {
   img.src = cycleImages[0];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[1];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[2];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[3];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   img.src = cycleImages[4];
-  await new Promise(r => setTimeout(r, 2000));
+  await new Promise((r) => setTimeout(r, 2000));
   cycle();
 }
 cycle();
 
+  console.log("after carousel")
+var cockTails = document.querySelector("ul");
+var searchBtn = document.querySelector("searchbtn");
 
-var cockTails = document.querySelector('ul');
-var searchBtn = document.querySelector('searchbtn')
+  console.log("after vars on line 28 and 29")
+
+const searchResultsArray = [
+  {
+    id: 1,
+    Cocktail: "",
+    ingredients: "brown",
+    favorite: 8,
+    pictureUrl:
+    "./images/2drinks.jpg,",
+    imageAlt: "your favorite cocktail"
+  },
+  {
+    id: 2,
+    Cocktail: "",
+   ingredients: "brown",
+    favorite: 8,
+    pictureUrl:
+      "./images/2drinks.jpg,",
+    imageAlt: "your favorite cocktail"
+  },
+  {
+    id: 3,
+   Cocktail: "",
+    ingredients: "brown",
+    favorite: 8,
+    pictureUrl:"./images/2drinks.jpg",
+    imageAlt: "your favorite cocktail"
+  },
+  {
+    id: 4,
+    Cocktail: "",
+    ingredients: "brown",
+    favorite: 8,
+    pictureUrl:"./images/2drinks.jpg,",
+    imageAlt: "your favorite cocktail"
+  }
+];
 
 // function to fetch api
-function getApi() {
-    var requestUrl = '';
+// function getApi(event) {
+//   var requestUrl = "https://www.thecocktaildb.com/api/json/v2/9973533/recent.php";
+  // var apikey = '9973533'
+  
+//   fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//           for (var i = 0; i < data.length; i++) {
+//               var listItem = document.createElement("li");
+//               listItem.textContent = data[i].html_url;
+//               cockTails.appendChild(listItem);
+//             }
+//         });
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; i < data.length; i++) {
-                var listItem = document.createElement('li');
-                listItem.textContent = data[i].html_url;
-                cockTails.appendChild(listItem);
-            }
-        });
-};
+//   event.preventDefault();
+//   document.getElementById('img').style.display = 'none';
+// }; 
 
-searchBtn.addEventListener('click', getApi);
+
+//searchBtn.addEventListener("click", jason);
+
+
+
+/////////////////////////////////////////////////////////////////////
+//Gretchen experiementing with fetch
+
+// var googleApiKey = (AIzaSyAHn5BUjeKUprcZG2E8m24ynnZIT2avTPk)
+// var googleUrl = 
+
+function jason(event) {
+  event.preventDefault();
+  document.querySelector('.description').style.display = 'none';
+
+}
+
+function start() {
+  // 2. Initialize the JavaScript client library.
+  gapi.client.init({
+    'apiKey': 'AIzaSyAHn5BUjeKUprcZG2E8m24ynnZIT2avTPk',
+    // Your API key will be automatically added to the Discovery Document URLs.
+    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
+    // clientId and scope are optional if auth is not required.
+    'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+    'scope': 'profile',
+  }).then(function() {
+    // 3. Initialize and make the API request.
+    return gapi.client.people.people.get({
+      'resourceName': 'people/me',
+      'requestMask.includeField': 'person.names'
+    });
+  }).then(function(response) {
+    console.log(response.result);
+  }, function(reason) {
+    console.log('Error: ' + reason.result.error.message);
+  });
+// 1. Load the JavaScript client library.
+gapi.load('client', start)};
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
