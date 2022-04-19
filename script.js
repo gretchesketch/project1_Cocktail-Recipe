621d96e477f75be7312c9e48f8edfbfcfa7c01b0
gretchesketch <gretchenjensen92@gmail.com>
2022-04-18 20:59:35 -0400

    got images to show up in both cards did some styling on card 2 to try and make it match the other card.

--- script.js
+++ script.js
@@ -22,42 +22,9 @@ async function cycle() {
   cycle();
 }
 cycle();
-console.log("after carousel");
-console.log("after vars on line 28 and 29");
-const searchResultsArray = [
-  {
-    id: 1,
-    Cocktail: "",
-    ingredients: "brown",
-    favorite: 8,
-    pictureUrl: "./images/2drinks.jpg,",
-    imageAlt: "your favorite cocktail",
-  },
-  {
-    id: 2,
-    Cocktail: "",
-    ingredients: "brown",
-    favorite: 8,
-    pictureUrl: "./images/2drinks.jpg,",
-    imageAlt: "your favorite cocktail",
-  },
-  {
-    id: 3,
-    Cocktail: "",
-    ingredients: "brown",
-    favorite: 8,
-    pictureUrl: "./images/2drinks.jpg",
-    imageAlt: "your favorite cocktail",
-  },
-  {
-    id: 4,
-    Cocktail: "",
-    ingredients: "brown",
-    favorite: 8,
-    pictureUrl: "./images/2drinks.jpg,",
-    imageAlt: "your favorite cocktail",
-  },
-];
+
+
+
 console.log("after search results array");
 function changeIcon() {
   if (document.getElementById("favbtn").textContent == "favorite_border")
@@ -80,44 +47,7 @@ var glass = document.getElementById("glass");
 var ingredients = document.getElementById("ingred");
 var instructions = document.getElementById("instruct");
 var cocktailSearch = document.querySelector("cocktailSearch"); // input value from user
-// var cocktailContainerEl = document.querySelector('cocktail')
 
-/////////////////// sample code from activity
-// var formSubmitHandler = function (event) {
-//   event.preventDefault();
-
-//   var cocktailResults = cocktailSearch.value.trim();
-
-//   if (cocktailResults) {
-//     getUserRepos(cocktailResults);
-
-//     cocktailContainerEl.textContent = '';
-//     cocktailSearch.value = '';
-//   } else {
-//     alert('Please enter a Cocktail name');
-//   }
-// };
-
-// var getCocktailRecipes = function (cocktailName) {
-//   var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + cocktailName;
-
-//   fetch(apiUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         console.log(response);
-//         response.json().then(function (data) {
-//           console.log(data);
-//           displayRepos(data, cocktailName);
-//         });
-//       } else {
-//         alert('Error: ' + response.statusText);
-//       }
-//     })
-//     .catch(function (error) {
-//       alert('Unable to connect to Database');
-//     });
-// };
-//////////////////
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
   var userSearch = document.getElementById("cocktailSearch").value;
@@ -138,7 +68,8 @@ searchBtn.addEventListener("click", function (event) {
     })
     .catch((error) => console.error("FETCH ERROR:", error));
 });
-// display cards
+// display card one.
+// Gets the data from the drink index
 function displayCocktail(data) {
   const cocktail = data.drinks[0];
   const cocktailDiv = document.getElementById("cocktail");
@@ -154,9 +85,13 @@ function displayCocktail(data) {
   cocktailDiv.appendChild(cocktailIngredients);
   const getIngredients = Object.keys(cocktail)
 
+// the filter method creates an array from an existing array.
+// In this case the array of drinks
+//proper syntax: .filter(function(item))
     .filter(function (ingredient) {
       return ingredient.indexOf("strIngredient") == 0;
     })
+    //Only display the ingredients that dont have a value of null
     .reduce(function (ingredients, ingredient) {
       if (cocktail[ingredient] != null) {
         ingredients[ingredient] = cocktail[ingredient];
@@ -169,19 +104,22 @@ function displayCocktail(data) {
     listItem = document.createElement("dt");
     listItem.innerHTML = value;
     cocktailIngredients.appendChild(listItem);
+  };
 
+    // Display Card two
+    // using inner HTML to display data from the index
     var cocktailEl = document.getElementById("cocktail1");
   var str1 = `
   <div class="card large"> 
-  <img src="${cocktailImg}">
-  <div class="card-content">
   <h3>${cocktailName}</h3>
+  <img id="cardTwo" src="${cocktailImg.src}">
+  <div class="card-content">
   <dl>
     <dt>Type of Glass:</dt>
     <dd id="glass1">${data.drinks[0].strGlass}</dd>
     <dt>Ingredients:</dt>
     <dd id="ingred1"></dd>
-    <dt>${cocktailIngredients.appendChild(listItem)}</dt>
+    <dt></dt>
     <dt>Instruction:</dt>
     <dd id="instruct1">${data.drinks[0].strInstructions}</dd>
   </dl>
@@ -194,5 +132,6 @@ function displayCocktail(data) {
 </div>
 </div> `;
   cocktailEl.innerHTML = str1;
-}};
+};
 
+const favoritesBar = document.getElementsByClassName("favorites-bar")
